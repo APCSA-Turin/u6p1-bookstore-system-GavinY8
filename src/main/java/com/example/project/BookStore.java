471962b5dc3b49647cf87f99b67c089321aa1420
GavinY8 <gaviny8@nycstudents.net>
2025-01-14 14:32:13 +0000

    unfinished

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -1,31 +1,79 @@
 package com.example.project;
 
-public class BookStore{
+public class BookStore {
 
     //requires at least 2 attributes Book[] books, User[] users (initialized to an empty array of 10 max users) 
+    private Book[] books;
+    private User[] users = new User[10];
+
 
     //requires 1 empty constructor
+    public BookStore() {}
 
-    // public getUsers(){}
+    public User[] getUsers() {
+        return users;
+    }
 
-    // public setUsers(){}
+    public void setUsers(User[] newUsers) {
+        users = newUsers;
+    }
 
-    // public  getBooks(){}
+    public Book[] getBooks() {
+        return books;
+    }
 
-    // public void addUser(User user){} 
+    public void addUser(User user) {
+        for (int i = 0; i < users.length; i++) {
+            //looks for the first avaliable empty index
+            if (users[i] == null) {
+                users[i] = user;
+                break;
+            }
+        }
+    } 
 
-    // public void removeUser(User user){}
+    public void removeUser(User user) {
+        for (int i = 0; i < users.length; i++) {
+            //check each index for a matching user
+            if (users[i] == user) {
+                // end loop after user is removed
+                users[i] = null;
+                break;
+            }
+        }
+    }
 
-    // public void consolidateUsers(){}
+    public void consolidateUsers() {
+        for (int i = 1; i < users.length; i++) {
+            if (users[i] != null) {
+                while (users[i-1] == null) {
+                    users[i-1] = users[i];
+                    users[i] = null;
+                    i -= 1;
+                }
+            }
+        }
+    }
 
-    // public void addBook(Book book){}
+    public void addBook(Book book) {
 
-    // public void insertBook(Book book, int index){}
+    }
 
-    // public void removeBook(Book book){}
+    public void insertBook(Book book, int index) {
+
+    }
+
+    public void removeBook(Book book) {
+
+    }
        
-    // public String bookStoreBookInfo(){} //you are not tested on this method but use it for debugging purposes
+    //you are not tested on this method but use it for debugging purposes
+    public String bookStoreBookInfo() {
+        return "";
+    }
 
-    // public String bookStoreUserInfo(){} //you are not tested on this method but use it for debugging purposes
-
+    //you are not tested on this method but use it for debugging purposes
+    public String bookStoreUserInfo() {
+        return "";
+    } 
 }
--- src/main/java/com/example/project/IdGenerate.java
+++ src/main/java/com/example/project/IdGenerate.java
@@ -3,14 +3,27 @@ package com.example.project;
 public class IdGenerate{ //This class contains statics variable and methods, you do not initalize an object to use it.
     
     // //requires 1 private attribute String currentId. You must initialize it to 99
+    private static String currentId = "99";
 
     // //requires one empty constructor
+    private IdGenerate() {}
+
+    public static String getCurrentId() {
+        return currentId;
+    }
+
+    public static void reset() {   //must reset the currentId back to 99
+        currentId = "99";
+    } 
 
 
-    // public  getCurrentId(){}
-
-    // public  reset(){} //must reset the currentId back to 99
-
-
-    // public generateID(){} //generates a new id, when called it will increment the currentId by 1.. Hint pay attention to data type of currentId
+    public static void generateID() {  //generates a new id, when called it will increment the currentId by 1.. Hint pay attention to data type of currentId
+        // Parse Id to int as it is a String
+        int id = Integer.parseInt(currentId);
+        //increment
+        id++;
+        //Convert new int value back to String
+        String newId = Integer.toString(id);
+        currentId = newId;
+    } 
 }
